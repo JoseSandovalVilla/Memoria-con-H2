037b8068fed255a905ffecd49bf436f42c4a797e
JoseSandovalVilla <jsvilla776@gmail.com>
2024-07-08 22:08:50 -0600

    Proyecto con Persistencia en Memoria con H2

---
+++ src/main/java/com/example/Tareaaa/TareaaaApplication.java
@@ -0,0 +1,13 @@
+package com.example.Tareaaa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TareaaaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TareaaaApplication.class, args);
+	}
+
+}
